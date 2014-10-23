ca709d3ffd413fca13cafadc0aad6bd400985ae8
Daniel Cheng <dcheng334@gmail.com>
2014-10-23 15:02:37 -0700

    CHG: Implements real gravity. DEL: extraneous files.

--- Stellar/Assets/Scripts/Gravity.cs
+++ Stellar/Assets/Scripts/Gravity.cs
@@ -1,24 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Applies gravitational force to all rigidbodies, with exception to those on the noGravity list.
+/// </summary>
+
 public class Gravity : MonoBehaviour {
 	public Transform planet;
+    public ArrayList noGravity;
 	public float magnitude = 9.8f;
-	public float gravitationalParameter = 3.9860044189f * Mathf.Pow(10.0f, 7.0f);
 
-	private Vector3 instantaneousAcceleration = new Vector3(0.0f, 0.0f, 0.0f);
-	private Vector3 earthDirection;
+	private float gravitationalParameter = 3.9860044189f * Mathf.Pow(10.0f, 7.0f);
+	private Vector3 planetDirection;
+    private ArrayList useGravity;
+
 	// Use this for initialization
 	void Start () {
-		
+        //search through entire hierarchy for top level rigidbodies, and store them in useGravity.
 	}
 	
 	// Update is called once per frame
-	void Update () {	
-		earthDirection = transform.position - planet.position;
-		Debug.DrawRay(transform.position, -earthDirection);
-		Debug.Log ("hhhehe");
-		rigidbody.velocity = rigidbody.velocity - Vector3.up * magnitude;
-
+	void Update () {
+        //for each transform in useGravity...
+        planetDirection = Vector3.Normalize(transform.position - planet.position);
+        rigidbody.velocity = rigidbody.velocity - planetDirection * magnitude * Time.deltaTime;
 	}
 }
