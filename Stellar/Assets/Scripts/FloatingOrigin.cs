738d2b18b70e50b01fce8d4df6ae59fde082599a
Daniel Cheng <dcheng334@gmail.com>
2014-10-25 10:56:20 -0700

    CHG: Gravity and FloatingOrigin are now compatible.

--- Stellar/Assets/Scripts/FloatingOrigin.cs
+++ Stellar/Assets/Scripts/FloatingOrigin.cs
@@ -7,24 +7,21 @@ public class FloatingOrigin : MonoBehaviour {
 	public Vector3 origin = new Vector3(0, 0, 0);
 	public Vector3 frameVelocity = new Vector3(0, 0, 0);
 	public Transform focus;
-	public float updateInterval = 0.1f;
 	
 	// Use this for initialization
 	void Start () {
 		//camera.nearClipPlane = 0.0001f;
+       
 	}
 	
 	// Update is called once per frame
-	void OnPostRender () {
-		Vector3 offset = Camera.main.transform.localPosition;
+	void Update () {
+        Vector3 offset = focus.position;
 		frameVelocity -= focus.rigidbody.velocity;
 		foreach (Transform topLevelObjects in transform.root){
-			if (topLevelObjects.rigidbody && focus != topLevelObjects){
-				topLevelObjects.rigidbody.velocity -= focus.rigidbody.velocity;
-			}
 			topLevelObjects.position -= offset;
 		}
-		focus.rigidbody.velocity = Vector3.zero;
+        Debug.Log(focus.rigidbody.velocity);
 		origin += offset;
 	}
 }
--- Stellar/Assets/Scripts/Gravity.cs
+++ Stellar/Assets/Scripts/Gravity.cs
@@ -7,22 +7,18 @@ using System.Collections;
 
 public class Gravity : MonoBehaviour {
 	public Transform planet;
-    public ArrayList noGravity;
 	public float magnitude = 9.8f;
 
 	private float gravitationalParameter = 3.9860044189f * Mathf.Pow(10.0f, 7.0f);
 	private Vector3 planetDirection;
-    private ArrayList useGravity;
 
 	// Use this for initialization
 	void Start () {
-        //search through entire hierarchy for top level rigidbodies, and store them in useGravity.
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        //for each transform in useGravity...
-        planetDirection = Vector3.Normalize(transform.position - planet.position);
-        rigidbody.velocity = rigidbody.velocity - planetDirection * magnitude * Time.deltaTime;
+	void FixedUpdate () {
+        planetDirection = -Vector3.Normalize(transform.position - planet.position);
+        Physics.gravity = planetDirection * magnitude;
 	}
 }
