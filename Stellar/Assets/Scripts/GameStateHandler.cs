39d12d37ef2fbed04e02e74ad3fedaeffc42500b
KTZimmerle <kzimmerl@uci.edu>
2014-10-31 02:42:54 -0700

    GameStateHandler.cs changes
    added in variables. Pushing.

--- Stellar/Assets/Scripts/GameStateHandler.cs
+++ Stellar/Assets/Scripts/GameStateHandler.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class GameStateHandler : MonoBehaviour {
 
+	
+	public float timeSinceStart = 0.0f;
+	public int gatesPassed = 0;
+
+	public ArrayList gateList;
+
 	// Use this for initialization
 	void Start () {
 	
