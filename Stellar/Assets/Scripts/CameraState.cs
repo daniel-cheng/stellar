4d402bd103002854e5788952a5686b24adb98fba
caileytay <cailey.mccabe@gmail.com>
2014-10-30 21:37:08 -0700

    Added CameraState.cs

--- Stellar/Assets/Scripts/CameraState.cs
+++ Stellar/Assets/Scripts/CameraState.cs
@@ -2,7 +2,10 @@
 using System.Collections;
 
 public class CameraState : MonoBehaviour {
-
+	//use to switch between camera states
+	private int stateIndex = 0;
+	//use to store all three camera states
+	public ArrayList cameraList;
 	// Use this for initialization
 	void Start () {
 	
