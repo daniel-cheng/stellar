5232c6a78f680b2d8504b81ba3ecb2aafaf01ded
Daniel Cheng <dcheng334@gmail.com>
2014-11-01 11:57:19 -0700

    CHG: Readded GameStateHandler.cs changes.

---
+++ Stellar/Assets/Scripts/GameStateHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStateHandler : MonoBehaviour {
+	public GameObject[] gateList;
+	public ArrayList gateList;
+
+	// Use this for initialization
+	void Start () {
+		gateList = GameObject.FindGameObjectsWithTag ("Gate");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
