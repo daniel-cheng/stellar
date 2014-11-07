3dad8c2e662ee7eb0854fced65fc1f1fa05845bf
binhphan421 <binhphan421@gmail.com>
2014-11-06 20:59:50 -0800

    WIP: sceneState, added Blue Danube, camera, audio

---
+++ Stellar/Assets/Scripts/SceneState.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SceneState : MonoBehaviour {
+	public AudioClip mainMenuMusic;
+	public List<string> sceneList = new List<string>{"main", "race", "trade"};
+	public int sceneIndex = 0;
+	CameraState cameraState;
+
+
+	// Use this for initialization
+	void Start () {
+		cameraState.cameraList [3].enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void onMouseDown()
+	{
+		cameraState.cameraList [3].enabled = false;
+		cameraState.cameraList [3].GetComponent<AudioSource>().enabled = false;
+	}
+}
