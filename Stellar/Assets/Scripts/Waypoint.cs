683d60c8618d4a22abde14294a16d9d7f149221f
caileytay <cailey.mccabe@gmail.com>
2014-11-11 21:55:55 -0800

    Waypoints

---
+++ Stellar/Assets/Scripts/Waypoint.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Waypoint : MonoBehaviour {
+
+	//transforms the arrow will be pointing at
+	public List<Transform> waypoints;
+	//current index of transform the arrow should be pointing at
+	public int waypointIndex = 0;
+	//object that will be pointed at
+	public Transform arrow;
+	//toggles whether or not arrow is visible
+	public bool enabled;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
