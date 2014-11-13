d57f4395d08c904794d63b2a7b78201c5d8dcf36
rwbysafire <tomtan117@gmail.com>
2014-11-12 16:35:21 -0800

    Updated GateStateHandler.cs

--- Stellar/Assets/Scripts/GateStateHandler.cs
+++ Stellar/Assets/Scripts/GateStateHandler.cs
@@ -2,6 +2,13 @@
 using System.Collections;
 
 public class GateStateHandler : MonoBehaviour {
+	public float distanceTravelled = 0.0f;
+	public float cargoCarried = 0.0f;
+	public float cargoDelivered = 0.0f;
+	public Transform tradingPostList;
+	public Transform tradingPostDestinationList;
+
+	private Vector2 cargoMassBounds = new Vector2(1000.0f, 100000.0f);
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +17,16 @@ public class GateStateHandler : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		float timeSinceStart = 0.0f;
+		timeSinceStart += Time.deltaTime;
+		distanceTravelled += rigidbody.velocity.magnitude * Time.deltaTime;
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other == tradingPostList){
+			cargoDelivered += cargoCarried;
+			cargoCarried = 0;
+		}
 	}
 }
+
