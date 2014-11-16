58aec2a0d3bdfc3c72a77af460c3026467ceeb27
KTZimmerle <kzimmerl@uci.edu>
2014-11-15 16:50:38 -0800

    KTZ: Added MouseAim.cs
    This script is fully operational. You can now use your mouse to point
    the ship where you want it to go. You can also click the check box
    titled "isEnabled" to toggle it on and off.

---
+++ Stellar/Assets/Scripts/MouseAim.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseAim : MonoBehaviour {
+
+	public bool isEnabled;
+
+	private float deltaX;
+	private float deltaY;
+	private float angleX;
+	private float angleY;
+	private Vector3 faceDirection = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		//if (isEnabled)
+		//	Screen.lockCursor = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (isEnabled) 
+		{
+			Debug.Log("nput.mousePosition.x: " + Input.mousePosition.x);
+			Debug.Log("nput.mousePosition.y: " + Input.mousePosition.y);
+
+			deltaX = Input.mousePosition.x - Screen.width/2;
+			deltaY = Input.mousePosition.y - Screen.height/2;
+
+			deltaX /= (Screen.width / 2);
+			deltaY /= (Screen.height / 2);
+
+			angleX = Mathf.Rad2Deg * Mathf.Asin (deltaX);
+			angleY = Mathf.Rad2Deg * Mathf.Asin (deltaY);
+
+
+			transform.eulerAngles = new Vector3(angleY, angleX, 0.0f);
+		}
+	}
+}
