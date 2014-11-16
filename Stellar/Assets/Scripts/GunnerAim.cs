f5e17a917c2dda638eee084f50756ed3533c96fd
Daniel Cheng <dcheng334@gmail.com>
2014-11-16 14:49:41 -0800

    Fixed GunnerAim.

--- Stellar/Assets/Scripts/GunnerAim.cs
+++ Stellar/Assets/Scripts/GunnerAim.cs
@@ -1,55 +1,55 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
-public class GunnerAim : MonoBehaviour {
-	
-	public bool isEnabled = true;
-	public Transform baseObject;
-	public Transform guns; 
-	
-	public float xSpeed = 250.0f;
+public class GunnerAim : MonoBehaviour
+{
+
+    public bool isEnabled = true;
+    public Transform baseObject;
+    public Transform guns;
+
+    public float xSpeed = 250.0f;
     public float ySpeed = 250.0f;
 
-	private Vector3 oldMousePosition;
-	private Vector3 deltaMousePosition;
+    private Vector3 oldMousePosition;
+    private Vector3 deltaMousePosition;
 
     private float x = 0.0f;
     private float y = 0.0f;
-    public float xMinLimit = -0;
-    public float xMaxLimit = 0;
-    public float yMinLimit = -0;
-    public float yMaxLimit = 0;
-	// Use this for initialization
-	void Start () {
-		oldMousePosition = Input.mousePosition;
-		deltaMousePosition = Vector3.zero;
+    public float xMinLimit = -0.0f;
+    public float xMaxLimit = 0.0f;
+    public float yMinLimit = -0.0f;
+    public float yMaxLimit = 0.0f;
+    // Use this for initialization
+    void Start()
+    {
+        oldMousePosition = Input.mousePosition;
+        deltaMousePosition = Vector3.zero;
 
-		var angles = transform.eulerAngles;
+        var angles = transform.eulerAngles;
         x = angles.y;
         y = angles.x;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (isEnabled) {
-			x = Input.GetAxis("Mouse X") * xSpeed * Time.deltaTime;
-			y = Input.GetAxis("Mouse Y") * ySpeed * Time.deltaTime;
-  
-			y = ClampAngle(y, yMinLimit, yMaxLimit);
-			x = ClampAngle(x, xMinLimit, xMaxLimit); 
-			baseObject.Rotate(Vector3.up * Time.deltaTime, x);
-            guns.Rotate(Vector3.left * Time.deltaTime, y);
-		}
-	}
-	
-	static float ClampAngle(float angle, float min, float max) {
-            if (angle < -360) {
-                angle += 360;
-            }
-            if (angle > 360) {
-                angle -= 360;
-            }
-            return Mathf.Clamp(angle, min, max);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isEnabled)
+        {
+            x += Input.GetAxis("Mouse X") * xSpeed * Time.deltaTime;
+            y += Input.GetAxis("Mouse Y") * ySpeed * Time.deltaTime;
+
+            Debug.Log(y.ToString());
+
+            x = Mathf.Clamp(x % 360.0f, xMinLimit, xMaxLimit);
+            y = Mathf.Clamp(y % 360.0f, 0.0f, 90.0f);
+
+            Debug.Log(y.ToString() + " " + yMinLimit.ToString() + " " + yMaxLimit.ToString());
+
+            //use mouse X to rotate around Y axis, mouse Y to rotate around X axis.
+            baseObject.localEulerAngles = new Vector3(0.0f, x, 0.0f);
+            guns.localEulerAngles = new Vector3(-y, 0.0f, 0.0f);
+
         }
-	
+    }
 }
