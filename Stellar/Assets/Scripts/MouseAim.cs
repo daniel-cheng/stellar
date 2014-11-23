f12337379963223c496781bca0efee293c452a20
KTZimmerle <kzimmerl@uci.edu>
2014-11-23 15:00:12 -0800

    Modified mouseAim.cs
    Turning the ship is now done on a continuously basis rather than an
    instantaneous basis. You basically point the mouse away from the middle
    of the screen to turn the ship around. Currently not inverted along
    y-axis. Watch out for deadlocking at the 90 degree angles of the y-axis
    of ship.

--- Stellar/Assets/Scripts/MouseAim.cs
+++ Stellar/Assets/Scripts/MouseAim.cs
@@ -9,12 +9,12 @@ public class MouseAim : MonoBehaviour {
 	private float deltaY;
 	private float angleX;
 	private float angleY;
-	private Vector3 faceDirection = Vector3.zero;
+	private float turnRateX;
+	private float turnRateY;
+	private Vector3 rateOfTurn = Vector3.zero;
 
 	// Use this for initialization
 	void Start () {
-		//if (isEnabled)
-		//	Screen.lockCursor = true;
 	}
 	
 	// Update is called once per frame
@@ -22,10 +22,7 @@ public class MouseAim : MonoBehaviour {
 	{
 		if (isEnabled) 
 		{
-			//Debug.Log("nput.mousePosition.x: " + Input.mousePosition.x);
-			//Debug.Log("nput.mousePosition.y: " + Input.mousePosition.y);
-
-			deltaX = Input.mousePosition.x - Screen.width/2;
+			/**/deltaX = Input.mousePosition.x - Screen.width/2;
 			deltaY = Input.mousePosition.y - Screen.height/2;
 
 			deltaX /= (Screen.width / 2);
@@ -33,11 +30,24 @@ public class MouseAim : MonoBehaviour {
 
 			angleX = Mathf.Rad2Deg * Mathf.Asin (deltaX) * 4.0f;
 			angleY = Mathf.Rad2Deg * Mathf.Asin (deltaY)  * 4.0f;
+			
+			/*angleX = Mathf.Rad2Deg * Mathf.Asin (deltaX);
+			angleY = Mathf.Rad2Deg * Mathf.Asin (deltaY);*/
+
+			turnRateX = angleX/90.0f;
+			turnRateY = angleY/90.0f;
 
             if (!float.IsNaN(angleX) && !float.IsNaN(angleY))
             {
-                transform.eulerAngles = new Vector3(-angleY, angleX, 0.0f);
+				rateOfTurn = transform.eulerAngles + new Vector3(-turnRateY, turnRateX, 0.0f);
+				transform.eulerAngles = rateOfTurn;
+				//transform.Rotate(new Vector3(-angleY, angleX, 0.0f));
             }
+
+			/*float distanceZ = (transform.position.z - Camera.main.transform.position.z)*1.0f;
+			Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distanceZ);
+			position = Camera.main.ScreenToWorldPoint(position);
+			transform.LookAt (position);*/
 		}
 	}
 }
