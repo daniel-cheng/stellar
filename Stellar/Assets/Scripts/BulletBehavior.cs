9ab34db8aabbf6f7db56c36c197e5a35f8ba5a86
Jonathan Mayer <jtmayer@uci.edu>
2015-01-26 00:53:46 -0800

    Implemented BulletBehavior.cs (still needs to be tested with HealthHandler.cs)

---
+++ Stellar/Assets/Scripts/BulletBehavior.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// This code has not been tested yet, and may contain bugs
+
+public class BulletBehavior : MonoBehaviour {
+	private float bulletDamage = 8.0f; // Sets damage to arbitrary value
+
+	void OnCollisionEnter(Collision collision)
+	{
+		GameObject objectHit = collision.gameObject; // Gets the GameObject of the collider the bullet collided with
+		if (objectHit.GetComponent<HealthHandler>() != null) // Checks if the GameOject has a HealthHandler (to be implemented)
+		{
+			objectHit.GetComponent<HealthHandler>().ModifyHealth(bulletDamage); // HealthHandler.ModifyHealth(float damage) needs to be implemented
+		}
+	}
+}
