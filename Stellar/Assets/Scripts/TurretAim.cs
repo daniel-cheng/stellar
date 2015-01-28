3d0e1fbd1679f14f41941ea1313b0d4ff89cc71c
Hyperlazydude <AnimeAzn101@gmail.com>
2015-01-27 19:19:49 -0800

    Added TurretAim.cs

---
+++ Stellar/Assets/Scripts/TurretAim.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TurretAim : MonoBehaviour {
+
+	//object that will be moving
+	public Transform turret;
+	//object that will be aimed at
+	public Transform fighter;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		turret.transform.LookAt(fighter);
+		}
+}
