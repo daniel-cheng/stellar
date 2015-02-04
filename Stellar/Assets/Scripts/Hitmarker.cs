4aa54060509351cec72fd22171cea899428d43ef
maddychan <madeline.chan.8@gmail.com>
2015-02-03 20:43:04 -0800

    Added Hitmarker - needs revision

---
+++ Stellar/Assets/Scripts/Hitmarker.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Hitmarker : MonoBehaviour{
+
+    public List<Transform> turretList;
+
+    public Transform arrow;
+
+    public bool enabled;
+    
+    void Start(){
+
+    }
+
+    void Update() {
+        //use lookAt()??
+    }
+}
+
+
