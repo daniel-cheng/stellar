6e1ee67de4923dfa34ed3e9d068fbe375bfc53f0
Daniel Cheng <dcheng334@gmail.com>
2015-02-04 21:19:26 -0800

    100% more bullet tracers.

--- Stellar/Assets/Scripts/Autogunner.cs
+++ Stellar/Assets/Scripts/Autogunner.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 
 public class Autogunner : MonoBehaviour {
+    public bool isEnabled;
+    public float velocity = 100;
+    public Transform newObject;
+    public Transform rootObject;
+    public float offset = 10.0f;
     
 	// Use this for initialization
 	void Start () {
@@ -10,6 +15,13 @@ public class Autogunner : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        GetComponent<ShootRound>().Shoot();
+        if (isEnabled)
+        {
+            Transform clone = Instantiate(newObject, rootObject.position + rootObject.up * offset, rootObject.rotation) as Transform;
+            clone.transform.parent = transform.root;
+            clone.rigidbody.velocity = clone.up * velocity;
+            //			Physics.IgnoreCollision(clone.collider, collider);
+            Destroy(clone.gameObject, 5.0f);
+        }
 	}
 }
