b476c205d731b3b30d5510e9e299de750766eb3d
Jonathan Mayer <jtmayer@uci.edu>
2015-02-08 22:58:58 -0800

    Turrets now fire only when player is within 5000 units.

--- Stellar/Assets/Scripts/Autogunner.cs
+++ Stellar/Assets/Scripts/Autogunner.cs
@@ -6,16 +6,14 @@ public class Autogunner : MonoBehaviour {
     public float velocity = 100;
     public Transform newObject;
     public Transform rootObject;
+	public Transform railgunObject;
+	public Transform fighter;
     public float offset = 10.0f;
-    
-	// Use this for initialization
-	void Start () {
-	    
-	}
+	public float maxRange = 5000.0f;
 	
 	// Update is called once per frame
 	void Update () {
-        if (isEnabled)
+		if (isEnabled && (Vector3.Distance(fighter.position, railgunObject.position) <= maxRange))
         {
             Transform clone = Instantiate(newObject, rootObject.position + rootObject.up * offset, rootObject.rotation) as Transform;
             clone.transform.parent = transform.root;
